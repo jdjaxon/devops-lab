b43f78be5da003070d7f9abb05ce045180a9d635
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-10-15 10:19:07 -0400

    Changed function names.

--- projects/server-stats/server-stats.sh
+++ projects/server-stats/server-stats.sh
@@ -3,15 +3,15 @@
 set -euo pipefail
 
 main() {
-    display_cpu_usage
-    display_memory_usage
-    display_disk_usage
-    display_top_proc_by_cpu
-    display_top_proc_by_mem
-    display_os_version
-    display_uptime
-    display_logged_in_users
-    display_auth_failures
+    get_cpu_usage
+    get_memory_usage
+    get_disk_usage
+    get_top_proc_by_cpu
+    get_top_proc_by_mem
+    get_os_version
+    get_uptime
+    get_logged_in_users
+    get_auth_failures
 }
 
 
@@ -26,13 +26,13 @@ print_section() {
 }
 
 
-display_cpu_usage() {
+get_cpu_usage() {
     cpu_usage=$(top -bn1 | awk '/%Cpu/ {usage=100 - $8; printf("%.2f", usage)}')
     print_section "CPU Usage" "$cpu_usage%"
 }
 
 
-display_memory_usage() {
+get_memory_usage() {
     mem_used_perc=$(free | awk '/Mem/ {printf("%.2f", $3/$2*100)}')
     mem_stats=$(free -h | \
         awk '/Mem/ {print "Total: " $2 "\nUsed: " $3 "\nFree: " $4}')
@@ -40,43 +40,43 @@ display_memory_usage() {
 }
 
 
-display_disk_usage() {
+get_disk_usage() {
     disk_usage=$(df -h --total)
     print_section "Disk Usage" "$disk_usage"
 }
 
 
-display_top_proc_by_cpu() {
+get_top_proc_by_cpu() {
     procs=$(ps -eo comm,pid,user,uid,pcpu,pmem --sort=-pcpu | head -n6)
     print_section "Top 5 Processes by CPU Usage" "$procs"
 }
 
 
-display_top_proc_by_mem() {
+get_top_proc_by_mem() {
     procs=$(ps -eo comm,pid,user,uid,pcpu,pmem --sort=-pmem | head -n6)
     print_section "Top 5 Processes by Memory Usage" "$procs"
 }
 
 
-display_os_version() {
+get_os_version() {
     os=$(uname -rsv)
     print_section "OS Version" "$os"
 }
 
 
-display_uptime() {
+get_uptime() {
     uptime=$(uptime)
     print_section "Uptime" "$uptime"
 }
 
 
-display_logged_in_users() {
+get_logged_in_users() {
     users=$(w)
     print_section "Active Users" "$users"
 }
 
 
-display_auth_failures() {
+get_auth_failures() {
     fails=""
     deb_logfile="/var/log/auth.log"
     rh_logfile="/var/log/secure"
